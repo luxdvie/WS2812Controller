b56cf697ca26cb4e439d4b92a27fd6a3b7e46ab5
Austin Brown <austin@brogencreations.com>
2017-11-18 21:10:35 -0500

    added christmas mode 1

--- app.js
+++ app.js
@@ -19,8 +19,7 @@
 		var color = 0x4b42f4;
 
 	/* Setup */
-
-		var MODES = { "INIT": 0, "FADE2": 1, "RAINBOW" : 2, "MANUAL" : 3, "CLEAR": 4, "DANCE": 5, "TWINKLE" : 6 };
+		var MODES = { "INIT": 0, "FADE2": 1, "RAINBOW" : 2, "MANUAL" : 3, "CLEAR": 4, "DANCE": 5, "TWINKLE" : 6, "XMAS1" : 7 };
 
 
 /*******************************
@@ -95,7 +94,6 @@
 	Web Methods
 *******************************/
 	
-
 	/*******************************
 		Home
 	*******************************/
@@ -199,259 +197,322 @@
 			StripStop();
 		});
 
+	/*******************************
+		Christmas Mode 1
+	*******************************/
+		app.get('/xmas1', function(request, response){
+			response.header("Access-Control-Allow-Origin", "*");
+			GoXmas1();
+			response.send("Going Xmas1");
+		});
 
 /*******************************
-	Fade 2 Colors
-*******************************/
-	var Fade2Color1;
-	var Fade2Color2;
-	var CurrentFadeColor;
-	var Brightness = 255;
-	var FadingDown = true;
-	var FadeSpeed = 10;
-	function GoFade2Colors(col1, col2) {
-		Fade2Color1 = parseInt("0x"+col1);
-		Fade2Color2 = parseInt("0x"+col2);
-		CurrentFadeColor = Fade2Color1;
-		SetStripColor(CurrentFadeColor);
-		if ($app.Mode != MODES.FADE2) {
-			$app.Mode = MODES.FADE2;
-			FadeTick();	
-		}		
-		$app.Mode = MODES.FADE2;
-		
-	}
-
-	function FadeTick() 
-	{
-		if ($app.Mode != MODES.FADE2) {
-			ws281x.setBrightness( 255 );
-			return;
-		}
-
-		if (FadingDown) {
-			// go down
-			if (Brightness <= 50) {
-				FadingDown = false;
-				if (CurrentFadeColor == Fade2Color1) {
-					CurrentFadeColor = Fade2Color2;
-				} else {
-					CurrentFadeColor = Fade2Color1;
-				}
-				SetStripColor( CurrentFadeColor );
-			} else {
-				// fade down
-				ws281x.setBrightness( Brightness );
-				Brightness--;
-			}
-		} else {
-			// go up
-			if (Brightness > 254) {
-				FadingDown = true;
-				
-			} else {
-				// fade down
-				ws281x.setBrightness( Brightness );
-				Brightness++;
-			} 
-		}
-		setTimeout(function () {
-			if ($app.Mode == MODES.FADE2) {
-				FadeTick();
-			} else {
-				FadeSpeed = 1000 / 30;
-				ws281x.setBrightness( 255 );
-			}
-		}, FadeSpeed);
-
-	}
-
-/*******************************
-	DANCE
+	Animation Groups
 *******************************/
 	
-	function GoDance() {
-		$app.Mode = MODES.DANCE;
-		DanceTick();
-	}
-
-	var DanceSpeed = 10;
-	function DanceTick() {
-		// colorWipe( 0xff3b00 , DanceSpeed); // Red
-		// theaterChase( 0xf8f9de , DanceSpeed); // White
-		// colorWipe( 0x0043ff , DanceSpeed); // Blue
-		// colorWipe( 0x1eff0a , DanceSpeed); // Green
-	    // theaterChase( 0xff3b00 , DanceSpeed); // Red
-	    // theaterChase( 0x0043ff , DanceSpeed); // Blue\
-			
-	    rainbowCycle(0, rainbowCycle);
-
-
-		setTimeout(function(){
-			if ( IsDancing() ) {
-				// DanceTick();
-			}
-		}, 10);
-
-	}
-
-	var rainbowCycleI, rainbowCycleJ = 0;
-	function rainbowCycle(wait, cb) {
-	 	
-		if ( rainbowCycleJ <  256*5 ) {
-			if ( rainbowCycleI < NUM_LEDS ) {
-                $app.Lights[rainbowCycleI] = Wheel(((rainbowCycleI * 256 / NUM_LEDS) + rainbowCycleJ) & 255);
-				rainbowCycleI++;
-				setTimeout( function(){ 
-					if (IsDancing()) {
-						rainbowCycle(wait, cb); 	
-					}					
-				}, wait );
-				StripRender();
-
-			} else {
-				StripRender();
-				rainbowCycleI = 0; 
-				rainbowCycleJ++;
-
-				setTimeout( function(){ 
-					if (IsDancing()) {
-						rainbowCycle(wait, cb); 	
-					}					
-				}, wait );
-
-			}
-		} else {
-			rainbowCycleI = 0;
-			rainbowCycleJ = 0;
-			if (cb) cb(wait, cb);
+	/*******************************
+		Christmas Mode 1
+	*******************************/
+		/*
+		*	Christmas mode 1. Cycle R / G / B / W
+		*/
+		function GoXmas1() {
+			$app.Mode = MODES.XMAS1;
+			Xmas1Tick();
 		}
 
-	}
+		var XmasRed = 0xff0000;
+		var XmasGreen = 0x00ff00;
+		var XmasBlue = 0x0000ff;
+		var XmasWhite = 0xffffff;
+		var Xmas1Speed = 750;
+		function Xmas1Tick() {	
 
-	function IsDancing() {
-		return $app.Mode == MODES.DANCE;
-	}
-
-	function Wheel(WheelPos) {
-	  if(WheelPos < 85) {
-	   return rgbToHex(WheelPos * 3, 255 - WheelPos * 3, 0);
-	  } else if(WheelPos < 170) {
-	   WheelPos -= 85;
-	   return rgbToHex(255 - WheelPos * 3, 0, WheelPos * 3);
-	  } else {
-	   WheelPos -= 170;
-	   return rgbToHex(0, WheelPos * 3, 255 - WheelPos * 3);
-	  }
-	}
-
-	function rgbToHex(r, g, b) {
-    	return parseInt("0x" + componentToHex(r) + componentToHex(g) + componentToHex(b));
-	}
-	function componentToHex(c) {
-    	var hex = c.toString(16);
-    	return hex.length == 1 ? "0" + hex : hex;
-	}
-
-
-/*******************************
-	Twinkle
-*******************************/
-	var WasTwinkling = false;
-	var TwinkleSpeed = 500;
-	function GoTwinkle() {
-		$app.Mode = MODES.TWINKLE;
-		WasTwinkling = false;
-		TwinkleTick();
-	}
-
-	var LastStates = [];
-
-	function GetNextColor(col, rand) {
-		var ind = TwinkleColors.indexOf(col);
-		if (ind == TwinkleColors.length + 1) {
-			// choose the first
-			return TwinkleColors[0];			
-		} else {
-			// choose the next
-			return TwinkleColors[ind+1];
-		}
-	}
-
-	 // = [ , 0xeaeaea, 0xd8d6d6, 0xbfbfbf, 0xa8a8a8, 0xE1E2DC, 0xc1c1bf, 0xa5a5a4, 0x898988, 0x616a5d  ];
-	 var TwinkleColors = [ 0xffffff, 0xFCFCFC, 0xFAFAFA, 0xF7F7F7, 0xF5F5F5, 0xF2F2F2, 0xF0F0F0, 0xEDEDED, 0xEBEBEB, 0xE8E8E8, 0xE5E5E5, 0xE3E3E3, 0xE0E0E0, 0xDEDEDE, 0xDBDBDB, 0xD9D9D9, 0xD6D6D6, 0xD4D4D4, 0xD1D1D1, 0xCFCFCF, 0xCCCCCC, 0xC9C9C9, 0xC7C7C7, 0xC4C4C4, 0xC2C2C2, 0xBFBFBF, 0xBDBDBD, 0xBABABA, 0xB8B8B8, 0xB5B5B5, 0xB3B3B3, 0xB0B0B0];
-
-	function TwinkleTick() {
-		if (!WasTwinkling) {
-			for (var x=0; x< NUM_LEDS;x++) {
-				// choose a random init point
-				var init = getRandomInt(0, TwinkleColors.length -1);
-				LastStates[x] = TwinkleColors[init]; // deafult white color
-				$app.Lights[x] = LastStates[x];
+			for (var index=0; index < NUM_LEDS; index++) {
+				var xmasLight = getRandomInt(1, 4);
+				var xmasColor = XmasRed;
+				switch (xmasLight) {
+					case 1: 
+						xmasColor = XmasRed;
+					break;
+					case 2: 
+						xmasColor = XmasGreen;
+					break;
+					case 3:
+						xmasColor = XmasBlue;
+					break;
+					case 4: 
+						xmasColor = XmasWhite;
+					break;
+				}
+								
+				$app.Lights[index] =  xmasColor;
 			}
+
 			StripRender();
-			WasTwinkling = true;	
-		} else {
 
-			for (var x=0; x< NUM_LEDS;x++) {
-				var shouldTwinkle = getRandomInt(0, 100);
-				if (shouldTwinkle > 10) {
-					// only a 50% chance of twinkling
-					var currentColor = LastStates[x];
-					var newColor = GetNextColor(currentColor);
-					LastStates[x] = newColor;
+			setTimeout(function () {
+				if ($app.Mode == MODES.XMAS1) {
+					Xmas1Tick();
+				} else {
+					Xmas1Speed = 1000 / 30;
+				}
+			}, Xmas1Speed);
+
+		}
+
+	/*******************************
+		Fade 2 Colors
+	*******************************/
+		var Fade2Color1;
+		var Fade2Color2;
+		var CurrentFadeColor;
+		var Brightness = 255;
+		var FadingDown = true;
+		var FadeSpeed = 10;
+		function GoFade2Colors(col1, col2) {
+			Fade2Color1 = parseInt("0x"+col1);
+			Fade2Color2 = parseInt("0x"+col2);
+			CurrentFadeColor = Fade2Color1;
+			SetStripColor(CurrentFadeColor);
+			if ($app.Mode != MODES.FADE2) {
+				$app.Mode = MODES.FADE2;
+				FadeTick();	
+			}		
+			$app.Mode = MODES.FADE2;
+			
+		}
+
+		function FadeTick() 
+		{
+			if ($app.Mode != MODES.FADE2) {
+				ws281x.setBrightness( 255 );
+				return;
+			}
+
+			if (FadingDown) {
+				// go down
+				if (Brightness <= 50) {
+					FadingDown = false;
+					if (CurrentFadeColor == Fade2Color1) {
+						CurrentFadeColor = Fade2Color2;
+					} else {
+						CurrentFadeColor = Fade2Color1;
+					}
+					SetStripColor( CurrentFadeColor );
+				} else {
+					// fade down
+					ws281x.setBrightness( Brightness );
+					Brightness--;
+				}
+			} else {
+				// go up
+				if (Brightness > 254) {
+					FadingDown = true;
+					
+				} else {
+					// fade down
+					ws281x.setBrightness( Brightness );
+					Brightness++;
+				} 
+			}
+			setTimeout(function () {
+				if ($app.Mode == MODES.FADE2) {
+					FadeTick();
+				} else {
+					FadeSpeed = 1000 / 30;
+					ws281x.setBrightness( 255 );
+				}
+			}, FadeSpeed);
+
+		}
+
+	/*******************************
+		Dance
+	*******************************/
+		
+		function GoDance() {
+			$app.Mode = MODES.DANCE;
+			DanceTick();
+		}
+
+		var DanceSpeed = 10;
+		function DanceTick() {
+			// colorWipe( 0xff3b00 , DanceSpeed); // Red
+			// theaterChase( 0xf8f9de , DanceSpeed); // White
+			// colorWipe( 0x0043ff , DanceSpeed); // Blue
+			// colorWipe( 0x1eff0a , DanceSpeed); // Green
+			// theaterChase( 0xff3b00 , DanceSpeed); // Red
+			// theaterChase( 0x0043ff , DanceSpeed); // Blue\
+				
+			rainbowCycle(0, rainbowCycle);
+
+
+			setTimeout(function(){
+				if ( IsDancing() ) {
+					// DanceTick();
+				}
+			}, 10);
+
+		}
+
+		var rainbowCycleI, rainbowCycleJ = 0;
+		function rainbowCycle(wait, cb) {
+			
+			if ( rainbowCycleJ <  256*5 ) {
+				if ( rainbowCycleI < NUM_LEDS ) {
+					$app.Lights[rainbowCycleI] = Wheel(((rainbowCycleI * 256 / NUM_LEDS) + rainbowCycleJ) & 255);
+					rainbowCycleI++;
+					setTimeout( function(){ 
+						if (IsDancing()) {
+							rainbowCycle(wait, cb); 	
+						}					
+					}, wait );
+					StripRender();
+
+				} else {
+					StripRender();
+					rainbowCycleI = 0; 
+					rainbowCycleJ++;
+
+					setTimeout( function(){ 
+						if (IsDancing()) {
+							rainbowCycle(wait, cb); 	
+						}					
+					}, wait );
+
+				}
+			} else {
+				rainbowCycleI = 0;
+				rainbowCycleJ = 0;
+				if (cb) cb(wait, cb);
+			}
+
+		}
+
+		function IsDancing() {
+			return $app.Mode == MODES.DANCE;
+		}
+
+		function Wheel(WheelPos) {
+		if(WheelPos < 85) {
+		return rgbToHex(WheelPos * 3, 255 - WheelPos * 3, 0);
+		} else if(WheelPos < 170) {
+		WheelPos -= 85;
+		return rgbToHex(255 - WheelPos * 3, 0, WheelPos * 3);
+		} else {
+		WheelPos -= 170;
+		return rgbToHex(0, WheelPos * 3, 255 - WheelPos * 3);
+		}
+		}
+
+		function rgbToHex(r, g, b) {
+			return parseInt("0x" + componentToHex(r) + componentToHex(g) + componentToHex(b));
+		}
+		function componentToHex(c) {
+			var hex = c.toString(16);
+			return hex.length == 1 ? "0" + hex : hex;
+		}
+
+
+	/*******************************
+		Twinkle
+	*******************************/
+		var WasTwinkling = false;
+		var TwinkleSpeed = 500;
+		function GoTwinkle() {
+			$app.Mode = MODES.TWINKLE;
+			WasTwinkling = false;
+			TwinkleTick();
+		}
+
+		var LastStates = [];
+
+		function GetNextColor(col, rand) {
+			var ind = TwinkleColors.indexOf(col);
+			if (ind == TwinkleColors.length + 1) {
+				// choose the first
+				return TwinkleColors[0];			
+			} else {
+				// choose the next
+				return TwinkleColors[ind+1];
+			}
+		}
+
+		// = [ , 0xeaeaea, 0xd8d6d6, 0xbfbfbf, 0xa8a8a8, 0xE1E2DC, 0xc1c1bf, 0xa5a5a4, 0x898988, 0x616a5d  ];
+		var TwinkleColors = [ 0xffffff, 0xFCFCFC, 0xFAFAFA, 0xF7F7F7, 0xF5F5F5, 0xF2F2F2, 0xF0F0F0, 0xEDEDED, 0xEBEBEB, 0xE8E8E8, 0xE5E5E5, 0xE3E3E3, 0xE0E0E0, 0xDEDEDE, 0xDBDBDB, 0xD9D9D9, 0xD6D6D6, 0xD4D4D4, 0xD1D1D1, 0xCFCFCF, 0xCCCCCC, 0xC9C9C9, 0xC7C7C7, 0xC4C4C4, 0xC2C2C2, 0xBFBFBF, 0xBDBDBD, 0xBABABA, 0xB8B8B8, 0xB5B5B5, 0xB3B3B3, 0xB0B0B0];
+
+		function TwinkleTick() {
+			if (!WasTwinkling) {
+				for (var x=0; x< NUM_LEDS;x++) {
+					// choose a random init point
+					var init = getRandomInt(0, TwinkleColors.length -1);
+					LastStates[x] = TwinkleColors[init]; // deafult white color
 					$app.Lights[x] = LastStates[x];
 				}
-			}
-			StripRender();
-		}
-		// dark color 616a5d
-
-		// lightest color 0xE1E2DC
-
-		setTimeout(function(){
-			if ( $app.Mode == MODES.TWINKLE ) {		
-				TwinkleTick();
-			}
-		}, TwinkleSpeed);
-		
-
-	}
-
-	function getRandomInt(min, max) {
-	    return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
-
-/*******************************
-	RAINBOW
-*******************************/
-
-	function GoRainbow() {
-		$app.Mode = MODES.RAINBOW;
-		RainbowTick();
-	}
-
-	var RainbowOffset = 0;
-	var RainbowSpeed = 1000 / 30;
-	function RainbowTick() 
-	{
-		for (var i = 0; i < NUM_LEDS; i++) {
-			pixelData[i] = colorwheel((RainbowOffset + i) % 256);
-		}
-
-		RainbowOffset = (RainbowOffset + 1) % 256;
-		ws281x.render(pixelData);
-		
-		setTimeout(function () {
-			if ($app.Mode == MODES.RAINBOW) {
-				RainbowTick();
+				StripRender();
+				WasTwinkling = true;	
 			} else {
-				ChooseNextMode();
-				RainbowOffset = 0;
+
+				for (var x=0; x< NUM_LEDS;x++) {
+					var shouldTwinkle = getRandomInt(0, 100);
+					if (shouldTwinkle > 10) {
+						// only a 50% chance of twinkling
+						var currentColor = LastStates[x];
+						var newColor = GetNextColor(currentColor);
+						LastStates[x] = newColor;
+						$app.Lights[x] = LastStates[x];
+					}
+				}
+				StripRender();
 			}
-		}, RainbowSpeed);
-	}
-	
+			// dark color 616a5d
+
+			// lightest color 0xE1E2DC
+
+			setTimeout(function(){
+				if ( $app.Mode == MODES.TWINKLE ) {		
+					TwinkleTick();
+				}
+			}, TwinkleSpeed);
+			
+
+		}
+
+		function getRandomInt(min, max) {			
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}
+
+	/*******************************
+		Rainbow
+	*******************************/
+
+		function GoRainbow() {
+			$app.Mode = MODES.RAINBOW;
+			RainbowTick();
+		}
+
+		var RainbowOffset = 0;
+		var RainbowSpeed = 1000 / 30;
+		function RainbowTick() 
+		{
+			for (var i = 0; i < NUM_LEDS; i++) {
+				pixelData[i] = colorwheel((RainbowOffset + i) % 256);
+			}
+
+			RainbowOffset = (RainbowOffset + 1) % 256;
+			ws281x.render(pixelData);
+			
+			setTimeout(function () {
+				if ($app.Mode == MODES.RAINBOW) {
+					RainbowTick();
+				} else {
+					ChooseNextMode();
+					RainbowOffset = 0;
+				}
+			}, RainbowSpeed);
+		}
+		
 
 /*******************************
 	Common
